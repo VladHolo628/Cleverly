c9ce82abc5e669acb51e98e2888446099a73294a
Vlad Shamov <holovlad@yandex.ru>
2021-04-18 12:12:38 +1000

    Добавляет сборку на gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
-const { dest, src } = require('gulp')
+const { dest, src, series, watch } = require('gulp')
 const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
 const csso = require('gulp-csso')
 const htmlMin = require('gulp-htmlmin')
+const imagemin = require('gulp-imagemin')
+const del = require('del')
 const sync = require('browser-sync').create()
+
+function html() {
+    return src('./src/**.html')
+        .pipe(htmlMin({
+            collapseWhitespace: true
+        }))
+        .pipe(dest('dist'))
+}
 
 function scss() {
     return src('./src/scss/**.scss')
@@ -12,4 +22,35 @@ function scss() {
         .pipe(csso())
         .pipe(dest('dist/css'))
 }
-exports.scss = scss
+
+function img() {
+    return src('src/img/**.{jpg,png,svg}')
+        .pipe(imagemin([
+            imagemin.mozjpeg({ quality: 75, progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+                plugins: [
+                    { removeViewBox: true },
+                    { cleanupIDs: false }
+                ]
+            })
+        ]))
+        .pipe(dest('dist/img'))
+}
+
+function clear() {
+    return del('dist')
+}
+
+function serve() {
+    sync.init({
+        server: './dist'
+    })
+
+    watch('src/**.html', html).on('change', sync.reload)
+    watch('src/scss/**.scss', scss).on('change', sync.reload)
+    watch('src/img/**.{jpg,png,svg}', img).on('change', sync.reload)
+}
+
+exports.serve = series(clear, scss, img, html, serve)
+exports.build = series(clear, scss, img, html)
